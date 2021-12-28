fb50c4a97d72edf1cde5b37d4c0dd2c81763f292
Christine Rowland <fordo29@gmail.com>
2021-12-28 11:34:19 -0700

    Add Treehouse tests for methods

--- test/Treehouse-test.js
+++ test/Treehouse-test.js
@@ -14,33 +14,56 @@ describe('Treehouse properties', function () {
     expect(treehouse).to.be.an.instanceof(Treehouse);
   });
 
-  it.skip('should store a price', function () {
+  it('should store a price', function () {
     const treehouse = new Treehouse(150);
     expect(treehouse.price).to.equal(150);
   });
 
-  it.skip('should store a price point with a default of an empty string', function () {
+  it('should store a price point with a default of an empty string', function () {
     const treehouse = new Treehouse(150);
     expect(treehouse.pricePoint).to.equal('');
   });
 
-  it.skip('should store the number of bedrooms', function () {
+  it('should store the number of bedrooms', function () {
     const treehouse = new Treehouse(150, 2);
     expect(treehouse.bedrooms).to.equal(2);
   });
 
-  it.skip('should store the number of bathrooms', function () {
+  it('should store the number of bathrooms', function () {
     const treehouse = new Treehouse(150, 2, 1);
     expect(treehouse.bathrooms).to.equal(1);
   });
 
-  it.skip('should store a location', function () {
+  it('should store a location', function () {
     const treehouse = new Treehouse(150, 2, 1, 'Aurora, CO');
     expect(treehouse.location).to.equal('Aurora, CO');
   });
 
-  it.skip('should default to the property is not booked', function () {
+  it('should default to the property is not booked', function () {
     const treehouse = new Treehouse(150, 2, 1, 'Aurora, CO');
     expect(treehouse.isBooked).to.equal(false);
+  });
+});
+
+describe('Treehouse methods', function () {
+
+  it.skip('should be able to calculate a reduction in price', function () {
+    const treehouse = new Treehouse(150, 2, 1, 'Aurora, CO');
+    expect(priceReduction(10)).to.equal(135);
+  });
+
+  it.skip('should update the price point based on price', function () {
+    const treehouse = new Treehouse(90, 2, 1, 'Aurora, CO');
+    expect(updatePricePoint(treehouse.price)).to.equal('$');
+  });
+
+  it.skip('should update the price point based on price', function () {
+    const treehouse = new Treehouse(150, 2, 1, 'Aurora, CO');
+    expect(updatePricePoint(treehouse.price)).to.equal('$$');
+  });
+
+  it.skip('should update the price point based on price', function () {
+    const treehouse = new Treehouse(250, 2, 1, 'Aurora, CO');
+    expect(updatePricePoint(treehouse.price)).to.equal('$$$');
   });
 });
