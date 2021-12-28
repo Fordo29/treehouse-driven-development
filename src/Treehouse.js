e99e7d4b5214498890be2b7dd50f52381db457ed
Christine Rowland <fordo29@gmail.com>
2021-12-28 12:11:34 -0700

    Add treehouse method functionality

--- src/Treehouse.js
+++ src/Treehouse.js
@@ -7,7 +7,20 @@ class Treehouse {
     this.location = location;
     this.isBooked = false;
   }
-  
+
+  priceReduction(reductionAmt) {
+    return this.price = (this.price -= (this.price / reductionAmt));
+  };
+
+  updatePricePoint() {
+    if (this.price <= 100) {
+      return this.pricePoint = '$';
+    } else if (this.price >= 200) {
+      return this.pricePoint = '$$$';
+    } else {
+      return this.pricePoint = '$$';
+    }
+  }
 }
 
 module.exports = Treehouse;
